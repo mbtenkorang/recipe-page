032b7b8131d67926471ea4f15e5c44144a71b5ee
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-19 19:18:47 +0000

    Adjust style for ingredientscomponent

--- src/components/IngredientsComponent.vue
+++ src/components/IngredientsComponent.vue
@@ -6,12 +6,12 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="mb-10 pb-6 border-b borger-wengebrown">
+    <div class="mb-10 pb-8 border-b borger-wengebrown">
         <h2 class="font-serif text-2xl text-nutmeg mb-6">Ingredients</h2>
-        <div>
-            <ul role="list" class="list-disc list-inside marker:text-nutmeg">
+        <div class="ml-4">
+            <ul role="list" class="list-disc list-outside marker:text-nutmeg">
                 <li v-for="(item,index) in ingredientsList" :key="index">
-                    <span class="ml-3 text-darkcharcoal">{{ item }}</span>
+                    <p class="ml-3 text-darkcharcoal">{{ item }}</p>
                 </li>
             </ul>
         </div>
