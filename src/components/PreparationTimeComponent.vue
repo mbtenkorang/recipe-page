bedbb6832bc0b3da28e06fd9354b82db6b5028f5
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-19 18:35:48 +0000

    Adjust styling for preparationTime component

--- src/components/PreparationTimeComponent.vue
+++ src/components/PreparationTimeComponent.vue
@@ -6,11 +6,11 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="my-10 py-4 px-2 bg-rosewhite rounded-lg">
-        <h2 class="text-xl font-bold my-3 ml-3 text-darkraspberry">Preparation time</h2>
-        <div class="ml-4 mb-4">
-            <ul role="list" class="list-disc list-inside marker:text-darkraspberry">
-                <li v-for="(value,key) in prepTime" :key="key" class="first-mt-0 last:mb-0 my-2 text-l">
+    <div class="my-10 py-4 px-3 bg-rosewhite rounded-lg">
+        <h2 class="text-xl font-bold my-4 ml-3 text-darkraspberry">Preparation time</h2>
+        <div class="ml-6 mb-4">
+            <ul role="list" class="list-disc list-outside marker:text-darkraspberry">
+                <li v-for="(value,key) in prepTime" :key="key" class="last:mb-0 mb-2">
                     <span class="font-bold ml-3">{{ key }}</span>: <span class="text-darkcharcoal">{{ value }}</span>
                 </li>
             </ul>
