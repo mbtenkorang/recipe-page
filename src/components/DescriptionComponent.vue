e4a696b3162c284e3823dd77684ecca7f73b27bd
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-19 15:07:53 +0000

    Styled component

--- src/components/DescriptionComponent.vue
+++ src/components/DescriptionComponent.vue
@@ -6,6 +6,6 @@ const props = defineProps({
 
 <template>
     <div>
-        <p class="font-sans">{{ description }}</p>
+        <p class="font-sans text-pretty">{{ description }}</p>
     </div>
 </template>
