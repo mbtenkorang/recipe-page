53471678e6ce18659013e04a5394e5d175f1e85e
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-19 19:25:51 +0000

    Adjust styling for instructionsComponent

--- src/components/InstructionsComponent.vue
+++ src/components/InstructionsComponent.vue
@@ -8,11 +8,10 @@ const props = defineProps({
 <template>
     <div class="mb-10 pb-6 border-b borger-wengebrown">
         <h2 class="font-serif text-2xl text-nutmeg mb-6">Preparation Time</h2>
-        <div>
-            <ol class="list-decimal list-inside marker:text-nutmeg text-darkcharcoal">
-                <li v-for="(value,key,index) in instructionSteps" :key="index+110">
-                    <p>{{ key }}:</p>
-                    <span>{{ value }}</span>
+        <div class="ml-4 mr-6">
+            <ol class="list-decimal list-outside marker:text-nutmeg text-darkcharcoal">
+                <li v-for="(value,key,index) in instructionSteps" :key="index+110" class=" mb-2 last:mb-0">
+                    <p class="text-pretty ml-1"><span class="font-bold">{{ key }}:</span> {{ value }}</p>
                 </li>
             </ol>
         </div>
