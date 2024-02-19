f7efa165bbfaba8274f4e682e813c753ab21001e
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-19 19:25:28 +0000

    Add spaces to recipeData.instructions

--- src/App.vue
+++ src/App.vue
@@ -36,10 +36,10 @@ const recipeData = reactive({
       "Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure\
     the eggs evenly coat the surface.",
     "Add fillings (optional)":
-      "When the eggs begin to set at the edges but are still slightly runny in the\
+      "When the eggs begin to set at the edges but are still slightly runny in the \
 middle, sprinkle your chosen fillings over one half of the omelette.",
     "Fold and serve":
-      "As the omelette continues to cook, carefully lift one edge and fold it over the\
+      "As the omelette continues to cook, carefully lift one edge and fold it over the \
 fillings. Let it cook for another minute, then slide it onto a plate.",
     "Enjoy": "Serve hot, with additional salt and pepper if needed.",
   },
