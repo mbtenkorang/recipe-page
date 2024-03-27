8e77d2fc97ea52cf122ddde10a936282b015d690
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-27 21:02:34 +0000

    Changed alt value for <img>

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -6,6 +6,6 @@ const props = defineProps({
 
 <template>
     <div class="md:pt-8 md:px-6 overflow-hidden">
-        <img :src="imagesrc" alt="image of omellette" class="md:rounded-lg">
+        <img :src="imagesrc" alt="Omelette with filling served on a plate" class="md:rounded-lg">
     </div>
 </template>
